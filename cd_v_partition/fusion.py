0856a4bb40e4c3a30d919b24450476f54698ef54
Nathaniel Hudson <nchudson88@gmail.com>
2024-01-03 13:21:56 -0500

    Type hinting and pydocs

--- cd_v_partition/fusion.py
+++ cd_v_partition/fusion.py
@@ -1,8 +1,10 @@
 import networkx as nx
 import numpy as np
 import itertools
+
 # Final fusion step to merge subgraphs
-# In infinite data limit this is done by screening for conflicting edges during union over subgraphs 
+# In infinite data limit this is done by screening for conflicting edges during union over subgraphs
+
 
 def screen_projections(partition, local_cd_adj_mats):
     """Fuse subgraphs by taking the union and resolving conflicts by favoring no edge over
@@ -10,7 +12,7 @@ def screen_projections(partition, local_cd_adj_mats):
 
     Args:
         partition (dict): the partition as a dictionary {comm_id : [nodes]}
-        local_cd_adj_mat (list[np.ndarray]): list of adjacency matrices for each local subgraph 
+        local_cd_adj_mat (list[np.ndarray]): list of adjacency matrices for each local subgraph
 
     Returns:
         nx.DiGraph: the final global directed graph with all nodes and edges
@@ -18,15 +20,20 @@ def screen_projections(partition, local_cd_adj_mats):
     # Take the union over graphs
     local_cd_graphs = _convert_local_adj_mat_to_graph(partition, local_cd_adj_mats)
     global_graph = _union_with_overlaps(local_cd_graphs)
-    
+
     # global_graph = no edge if (no edge in comm1) or (no edge in comm2)
     for comm, adj_comm in zip(partition.values(), local_cd_adj_mats):
-        for row, col in itertools.product(np.arange(adj_comm.shape[0]),np.arange(adj_comm.shape[0])):
+        for row, col in itertools.product(
+            np.arange(adj_comm.shape[0]), np.arange(adj_comm.shape[0])
+        ):
             i = comm[row]
             j = comm[col]
-            if (not adj_comm[row,col] and not adj_comm[col,row]) and global_graph.has_edge(i,j):
-                global_graph.remove_edge(i,j)
+            if (
+                not adj_comm[row, col] and not adj_comm[col, row]
+            ) and global_graph.has_edge(i, j):
+                global_graph.remove_edge(i, j)
     return global_graph
+
 
 def fusion(partition, local_cd_adj_mats, data, cov):
     """Fuse subgraphs by taking the union and resolving conflicts by taking the lower
@@ -34,33 +41,36 @@ def fusion(partition, local_cd_adj_mats, data, cov):
 
     Args:
         partition (dict): the partition as a dictionary {comm_id : [nodes]}
-        local_cd_adj_mat (list[np.ndarray]): list of adjacency matrices for each local subgraph 
+        local_cd_adj_mat (list[np.ndarray]): list of adjacency matrices for each local subgraph
 
     Returns:
         nx.DiGraph: the final global directed graph with all nodes and edges
     """
     # Convert adjacency matrices to nx.DiGraphs, make sure to label nodes using the partition
     local_cd_graphs = _convert_local_adj_mat_to_graph(partition, local_cd_adj_mats)
-    
+
     # Take the union over graphs
     global_graph = _union_with_overlaps(local_cd_graphs)
-    
-    global_graph_resolved = global_graph.copy() # TODO this is an expensive copy 
-    for (i,j) in global_graph.edges():
-        if global_graph.has_edge(j,i):
+
+    global_graph_resolved = global_graph.copy()  # TODO this is an expensive copy
+    for i, j in global_graph.edges():
+        if global_graph.has_edge(j, i):
             #  Resolve conflicts by favoring lower ric_scores
-            if global_graph_resolved.has_edge(j,i):
-                global_graph_resolved.remove_edge(j,i)
-            if global_graph_resolved.has_edge(i,j):
-                global_graph_resolved.remove_edge(i,j)
-            
+            if global_graph_resolved.has_edge(j, i):
+                global_graph_resolved.remove_edge(j, i)
+            if global_graph_resolved.has_edge(i, j):
+                global_graph_resolved.remove_edge(i, j)
+
             pa_i = list(global_graph_resolved.predecessors(i))
             pa_j = list(global_graph_resolved.predecessors(j))
-            edge = _resolve_w_ric_score(global_graph_resolved, data, cov, i, j, pa_i, pa_j)
-            
+            edge = _resolve_w_ric_score(
+                global_graph_resolved, data, cov, i, j, pa_i, pa_j
+            )
+
             if edge:
                 global_graph_resolved.add_edge(edge[0], edge[1])
     return global_graph_resolved
+
 
 def fusion_basic(partition, local_cd_adj_mats):
     """Fuse subgraphs by taking the union and resolving conflicts by taking the higher
@@ -69,30 +79,31 @@ def fusion_basic(partition, local_cd_adj_mats):
 
     Args:
         partition (dict): the partition as a dictionary {comm_id : [nodes]}
-        local_cd_adj_mat (list[np.ndarray]): list of adjacency matrices for each local subgraph 
+        local_cd_adj_mat (list[np.ndarray]): list of adjacency matrices for each local subgraph
 
     Returns:
         nx.DiGraph: the final global directed graph with all nodes and edges
     """
     # Convert adjacency matrices to nx.DiGraphs, make sure to label nodes using the partition
     local_cd_graphs = _convert_local_adj_mat_to_graph(partition, local_cd_adj_mats)
-    
+
     # Take the union over graphs
     global_graph = _union_with_overlaps(local_cd_graphs)
-    
+
     #  Resolve conflicts by favoring higher weights
-    global_graph_resolved = global_graph.copy() # TODO this is an expensive copy 
-    for (i,j) in global_graph.edges():
-        if global_graph.has_edge(j,i):
-            weight_ij = global_graph.get_edge_data(i,j)['weight']
-            weight_ji = global_graph.get_edge_data(j,i)['weight']
+    global_graph_resolved = global_graph.copy()  # TODO this is an expensive copy
+    for i, j in global_graph.edges():
+        if global_graph.has_edge(j, i):
+            weight_ij = global_graph.get_edge_data(i, j)["weight"]
+            weight_ji = global_graph.get_edge_data(j, i)["weight"]
             print("Conflict found, weights: {} {}".format(weight_ij, weight_ji))
             if weight_ij > weight_ji:
-                global_graph_resolved.remove_edge(j,i)
+                global_graph_resolved.remove_edge(j, i)
             else:
-                global_graph_resolved.remove_edge(i,j)
+                global_graph_resolved.remove_edge(i, j)
 
     return global_graph_resolved
+
 
 def _convert_local_adj_mat_to_graph(partition, local_cd_adj_mats):
     """
@@ -100,18 +111,21 @@ def _convert_local_adj_mat_to_graph(partition, local_cd_adj_mats):
     method) into networkx DiGraphs using the correct global node index for each partition.
     """
     local_cd_graphs = []
-    for part,adj in zip(partition.items(), local_cd_adj_mats):
+    for part, adj in zip(partition.items(), local_cd_adj_mats):
         _, node_ids = part
         subgraph = nx.from_numpy_array(adj, create_using=nx.DiGraph)
-        nx.relabel_nodes(subgraph, mapping=dict(zip(np.arange(len(node_ids)), node_ids)),copy=False)
+        nx.relabel_nodes(
+            subgraph, mapping=dict(zip(np.arange(len(node_ids)), node_ids)), copy=False
+        )
         local_cd_graphs.append(subgraph)
     return local_cd_graphs
+
 
 def _union_with_overlaps(graphs):
     """
     Helper function that reimplements networkx.union_all, except remove the
     requirement that the node sets be disjoint ie we allow for overlapping nodes/edges
-    between graphs 
+    between graphs
     """
     R = None
     seen_nodes = set()
@@ -132,14 +146,16 @@ def _union_with_overlaps(graphs):
 
     return R
 
+
 # For a candidate added edge (u,v)
-# if the path v->u exists is the graph, then adding the edge will create a cycle 
+# if the path v->u exists is the graph, then adding the edge will create a cycle
 def _detect_cycle(G, edge):
     has_path = nx.has_path(G, edge[1], edge[0])
     return has_path
 
+
 # From paper https://www.jmlr.org/papers/v21/19-318.html
-# Choose no edge, i->j, or j->i based on local RIC score 
+# Choose no edge, i->j, or j->i based on local RIC score
 # Only add an edge if the RIC score for i->j and j->i both are greater than the score with no edge
 # max(RIC(i->j), RIC(j->i)) < RIC(i,j))
 def _resolve_w_ric_score(G, data, cov, i, j, pa_i, pa_j):
@@ -149,26 +165,27 @@ def _resolve_w_ric_score(G, data, cov, i, j, pa_i, pa_j):
     l_ji = _fast_logpdf(data, i, pa_i + [j], cov)
     p = data.shape[1]
     n = data.shape[0]
-    lam = np.log(n) if p <= np.sqrt(n) else 2* np.log(p)
+    lam = np.log(n) if p <= np.sqrt(n) else 2 * np.log(p)
 
-    add_edge = 2*np.min([l_ij - l_0j, l_ji - l_0i]) > lam
-    
+    add_edge = 2 * np.min([l_ij - l_0j, l_ji - l_0i]) > lam
+
     # Choose the edge that does not result in a cycle, otherwise choose the
     # minimal scoring edge
     if add_edge:
-        if _detect_cycle(G, (i,j)):
-            return (j,i)
-        elif _detect_cycle(G, (j,i)):
-            return (i,j)
+        if _detect_cycle(G, (i, j)):
+            return (j, i)
+        elif _detect_cycle(G, (j, i)):
+            return (i, j)
         elif l_ji > l_ij:
-            return (i,j)
+            return (i, j)
         else:
-            return (j,i)
+            return (j, i)
     else:
         return None
-    
+
+
 def _fast_logpdf(samples, node, parents, covariance):
-    """Calculate the likelihood of a set of nodes and candidate parents for Gaussian variables.  
+    """Calculate the likelihood of a set of nodes and candidate parents for Gaussian variables.
     TODO where does this eq come from. The code is ported from graphical_models.GaussDAG.fast_logpdf
 
     Args:
@@ -183,7 +200,7 @@ def _fast_logpdf(samples, node, parents, covariance):
     cov_pn = covariance[np.ix_(parents, [node])]
     cov_pp = covariance[np.ix_(parents, parents)]
     rss = cov_nn - cov_pn.T.dot(np.linalg.inv(cov_pp)).dot(cov_pn)
-    
+
     # vals, vecs = np.linalg.eigh(rss)
     # logdet     = np.sum(np.log(vals))
     # valsinv    = 1./vals
@@ -195,7 +212,4 @@ def _fast_logpdf(samples, node, parents, covariance):
     # log2pi     = np.log(2 * np.pi)
     # return -0.5 * (dim * log2pi + maha + logdet)
     N = samples.shape[0]
-    return 0.5 * (- N * (np.log(2 * np.pi) + 1 - np.log(N) + np.log(rss*(N-1))));
-
-
-    
+    return 0.5 * (-N * (np.log(2 * np.pi) + 1 - np.log(N) + np.log(rss * (N - 1))))
--- cd_v_partition/overlapping_partition.py
+++ cd_v_partition/overlapping_partition.py
@@ -1,14 +1,15 @@
 import numpy as np
 import subprocess
 
-def oslom_algorithm(nodes, data_dir, oslom_dir, structure_type='dag'):
+
+def oslom_algorithm(nodes, data_dir, oslom_dir, structure_type="dag"):
     """Overlapping partitioning methods which take an input graph (superstructure) and partition nodes according to an objective
        overlapping nodes ideally render the partitions conditionally independent
 
     Args:
         data_dir (str): the directory containing the *.dat file which holds the edges of the structure to partition
-        oslom_dir (str): the directory containing the OSLOM binary 
-        structure_type (str, optional): specify the structure type as either the 'dag', 
+        oslom_dir (str): the directory containing the OSLOM binary
+        structure_type (str, optional): specify the structure type as either the 'dag',
                                         'superstructure', or 'superstructure_weighted'. If weighted
                                         then weights in the *.dat are used by OSLOM. Defaults to 'dag'.
 
@@ -17,13 +18,20 @@ def oslom_algorithm(nodes, data_dir, oslom_dir, structure_type='dag'):
     """
     # Run the OSLOM code externally
     weight_flag = "-w" if "weight" in structure_type else "-uw"
-    subprocess.run(["{}/oslom_undir".format(oslom_dir), "-f", 
-                    "{}/edges_{}.dat".format(data_dir, structure_type),"{}".format(weight_flag)]
-                   ,stdout=subprocess.DEVNULL,stderr=subprocess.STDOUT)
-    
-    # Read the output partition file and return the partition as a dictionary 
+    subprocess.run(
+        [
+            "{}/oslom_undir".format(oslom_dir),
+            "-f",
+            "{}/edges_{}.dat".format(data_dir, structure_type),
+            "{}".format(weight_flag),
+        ],
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.STDOUT,
+    )
+
+    # Read the output partition file and return the partition as a dictionary
     partition_file = "{}/edges_{}.dat_oslo_files/tp".format(data_dir, structure_type)
-    with open(partition_file, 'rb') as f:
+    with open(partition_file, "rb") as f:
         lines = f.readlines()
     lines = lines[1::2]
     lines = [[int(node) for node in l.split()] for l in lines]
@@ -37,24 +45,22 @@ def oslom_algorithm(nodes, data_dir, oslom_dir, structure_type='dag'):
         partition[len(lines)] = homeless_nodes
     return partition
 
+
 def partition_problem(partition, structure, data):
     """Split a the graph structure and dataset according to the given graph partition
 
     Args:
         partition (dict): the partition as a dictionary {comm_id : [nodes]}
-        structure (np.ndarray): the adjacency matrix for the initial structure 
+        structure (np.ndarray): the adjacency matrix for the initial structure
         data (pandas DataFrame): the dataset, columns correspond to nodes in the graph
 
     Returns:
-        list: a list of tuples holding the sub structure and data subsets for each partition 
+        list: a list of tuples holding the sub structure and data subsets for each partition
     """
     sub_problems = []
     for _, sub_nodes in partition.items():
-        sub_structure = structure[sub_nodes][:,sub_nodes]
-        data_inds = sub_nodes + [-1] # add 'target' vector at the end of dataframe
-        sub_data = data.iloc[:,data_inds]
+        sub_structure = structure[sub_nodes][:, sub_nodes]
+        data_inds = sub_nodes + [-1]  # add 'target' vector at the end of dataframe
+        sub_data = data.iloc[:, data_inds]
         sub_problems.append((sub_structure, sub_data))
     return sub_problems
-        
-    
-    
