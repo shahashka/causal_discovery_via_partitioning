12c32ce5500c9c731f272d32a58a1048b125006e
Ashka Shah <shahashka@lambda4.cels.anl.gov>
2024-01-18 18:07:50 -0800

    add candidate set to fusion algorithm:

--- tests/empirical_tests/test_causal_partition.py
+++ tests/empirical_tests/test_causal_partition.py
@@ -7,7 +7,9 @@ from cd_v_partition.utils import (
     get_data_from_graph,
     delta_causality,
     edge_to_adj,
-    create_k_comms
+    create_k_comms,
+    artificial_superstructure,
+    get_scores
 )
 from cd_v_partition.causal_discovery import sp_gies, pc
 from cd_v_partition.fusion import screen_projections, fusion, fusion_basic
@@ -49,8 +51,11 @@ def run_causal_discovery(superstructure, partition, df, G_star):
     est_graph_serial = _local_structure_learn([superstructure, df])
 
     # Compare causal metrics
-    d_scores = delta_causality(est_graph_serial, est_graph_partition, G_star)
-    return d_scores[-2]  # this is the delta true positive rate
+    # d_scores = delta_causality(est_graph_serial, est_graph_partition, G_star)
+    scores_serial = get_scores(["CD-serial"], [est_graph_serial], G_star)
+    scores_part = get_scores(["CD-partition"], [est_graph_serial], G_star)
+
+    return scores_serial[-2], scores_part[-1]  # this is the  true positive rate
 
 def vis(name, partition, superstructure):
     superstructure = adj_to_dag(superstructure)
@@ -58,40 +63,12 @@ def vis(name, partition, superstructure):
                           partition=partition, save_name="./tests/empirical_tests/{}_partition.png".format(name))
 
 
-def artificial_superstructure(
-    G_star_adj_mat, frac_retain_direction=0.1, frac_extraneous=0.5
-):
-    """Creates a superstructure by discarding some of the directions in edges of G_star and adding
-    extraneous edges.
-
-    Args:
-        G_star_adj_mat (np.ndarray): the adjacency matrix for the target graph
-        frac_retain_direction (float): what percentage of edges will retain their direction information
-        frac_extraneous (float): adds frac_extraneous*m many additional edges, for m the number of edges in G_star
-
-    Returns:
-        super_adj_mat (np.ndarray): an adjacency matrix for the superstructure we've created
-    """
-    G_star = nx.from_numpy_array(G_star_adj_mat, create_using=nx.DiGraph())
-    true_edge_set = set(G_star.edges())
-
-    # returns a deepcopy
-    G_super = G_star.to_undirected()
-    # add extraneous edges
-    m = G_star.number_of_edges()
-    nodes = list(G_star.nodes())
-    G_super.add_edges_from(pick_k_random_edges(k=int(frac_extraneous * m), nodes=nodes))
-
-    return nx.adjacency_matrix(G_super).toarray()
-
-def pick_k_random_edges(k, nodes):
-    return list(zip(random.choices(nodes, k=k), random.choices(nodes, k=k)))
-
 
 def run():
-    num_repeats = 30
+    num_repeats = 10
     sample_range = [1e2, 1e3, 1e4, 1e5, 1e6, 1e7]
     alpha=0.5
+    scores_serial = np.zeros((num_repeats, len(sample_range)))
     scores_edge_cover = np.zeros((num_repeats, len(sample_range)))
     scores_hard_partition = np.zeros((num_repeats, len(sample_range)))
     scores_causal_partition = np.zeros((num_repeats, len(sample_range)))
@@ -114,33 +91,30 @@ def run():
                 iv_samples=0,bias=bias, var=var
             )
             G_star = edge_to_adj(edges, nodes)
-            #superstructure = artificial_superstructure(G_star, frac_extraneous=0.1)
-            data_obs = df.drop(columns=["target"]).to_numpy()
-            superstructure, _ = pc(data_obs, alpha=alpha, outdir=None)
+            superstructure = artificial_superstructure(G_star, frac_extraneous=0.1)
+            #data_obs = df.drop(columns=["target"]).to_numpy()
+            #superstructure, _ = pc(data_obs, alpha=alpha, outdir=None)
 
 
-            d_tpr_hard = run_causal_discovery(superstructure, init_partition, df, G_star)
+            ss, sp = run_causal_discovery(superstructure, init_partition, df, G_star)
             vis("init", init_partition, G_star)
-            scores_hard_partition[i][j] = d_tpr_hard
-            print(init_partition)
+            scores_serial[i][j] = ss
+            scores_hard_partition[i][j] = sp
 
             partition = rand_edge_cover_partition(superstructure, init_partition)
             vis("edge_cover", partition, G_star)
-            d_tpr_ec = run_causal_discovery(superstructure, partition, df, G_star)
-            scores_edge_cover[i][j] = d_tpr_ec
-            print(partition)
+            _, sp = run_causal_discovery(superstructure, partition, df, G_star)
+            scores_edge_cover[i][j] = sp
             
             partition = expansive_causal_partition(superstructure, init_partition)
             vis("causal", partition, G_star)
-            d_tpr_cp = run_causal_discovery(superstructure, partition, df, G_star)
-            scores_causal_partition[i][j] = d_tpr_cp
-            print(partition)
+            _, sp = run_causal_discovery(superstructure, partition, df, G_star)
+            scores_causal_partition[i][j] = sp
             
             partition = modularity_partition(superstructure, cutoff=1, best_n=2)
             vis("mod", partition, G_star)
-            d_tpr_mod = run_causal_discovery(superstructure, partition, df, G_star)
-            scores_mod_partition[i][j] = d_tpr_mod
-            print(partition)
+            _, sp = run_causal_discovery(superstructure, partition, df, G_star)
+            scores_mod_partition[i][j] = sp
 
     labels = []
 
@@ -150,6 +124,10 @@ def run():
 
     plt.clf()
     _, ax = plt.subplots()
+    add_label(
+        ax.violinplot(scores_serial, showmeans=True, showmedians=False),
+        label="serial",
+    )
     add_label(
         ax.violinplot(scores_edge_cover, showmeans=True, showmedians=False),
         label="edge_cover",
@@ -176,7 +154,7 @@ def run():
     ax.set_title("Comparison of partition types for 2 community scale free networks")
     plt.legend(*zip(*labels), loc=2)
     plt.savefig(
-        "./tests/empirical_tests/causal_part_test_ss.png"
+        "./tests/empirical_tests/causal_part_test_artificial_ss.png"
     )
 
 
