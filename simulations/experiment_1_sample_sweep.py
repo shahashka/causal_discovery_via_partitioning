59bce17c0a4f979c417bb24f26fb3124fd59dcda
shahashka <45748060+shahashka@users.noreply.github.com>
2024-02-04 09:09:58 -0600

    Update experiment_1_sample_sweep.py

--- simulations/experiment_1_sample_sweep.py
+++ simulations/experiment_1_sample_sweep.py
@@ -131,7 +131,8 @@ def run_samples_alg(
 if __name__ == "__main__":
 
     # Simple case for debugging
-    algorithms = ["serial", "pef", "edge_cover", "expansive_causal", "mod"]
+    #algorithms = ["serial", "pef", "edge_cover", "expansive_causal", "mod"]
+    algorithms = ["pef"]
     # func_partial = functools.partial(run_samples_alg, experiment_dir="./simulations/experiment_1_test/", nthreads=16, num_repeats=2, sample_range=[10**i for i in np.arange(1,3)], screen=True )
     # results = []
     # with ProcessPoolExecutor(max_workers=len(algorithms)) as executor:
@@ -139,18 +140,18 @@ if __name__ == "__main__":
     #         results.append(result)
     
     #screen projections
-    func_partial = functools.partial(
-        run_samples_alg,
-        experiment_dir="./simulations/experiment_1/",
-        nthreads=16,
-        num_repeats=30,
-        nnodes_range=[10**i for i in np.arange(1, 6)],
-        screen=True,
-    )
-    results = []
-    with ProcessPoolExecutor(max_workers=len(algorithms)) as executor:
-        for result in executor.map(func_partial, algorithms, chunksize=1):
-            results.append(result)
+    # func_partial = functools.partial(
+    #     run_samples_alg,
+    #     experiment_dir="./simulations/experiment_1/",
+    #     nthreads=16,
+    #     num_repeats=30,
+    #     nnodes_range=[10**i for i in np.arange(1, 6)],
+    #     screen=True,
+    # )
+    # results = []
+    # with ProcessPoolExecutor(max_workers=len(algorithms)) as executor:
+    #     for result in executor.map(func_partial, algorithms, chunksize=1):
+    #         results.append(result)
             
     
     # fusion    
